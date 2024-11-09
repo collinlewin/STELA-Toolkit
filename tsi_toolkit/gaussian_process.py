44c5f1585f40d178637ddd65d49a05977b53e377
collinlewin <clewin@mit.edu>
2024-11-09 11:55:52 -0500

    Implemented train, predict functions

--- tsi_toolkit/gaussian_process.py
+++ tsi_toolkit/gaussian_process.py
@@ -1,4 +1,189 @@
 import torch
 import gpytorch
+import numpy as np
 
+from .data_loader import TimeSeries
 
+# to do: consider noise prior
+class GaussianProcess(gpytorch.models.ExactGP):
+    def __init__(self, kernel_form, timeseries=None, train_t=[], train_y=[], train_e=[], white_noise=True, run_training=True, train_iter=100, learn_rate=0.1, verbose=False):
+
+        if timeseries:
+            if isinstance(timeseries, TimeSeries):
+                self.timeseries = timeseries
+            else:
+                raise TypeError("Expected timeseries to be a TimeSeries object.")
+        
+        elif train_t and train_y:
+            self.timeseries = TimeSeries(times=train_t, values=train_y, errors=train_e)
+        
+        else:
+            raise ValueError(
+                "Please provide either a TimeSeries object as 'timeseries' "
+                "or arrays for 'train_t' and 'train_y' (and optionally 'train_e')."
+            )
+    
+        # Standardize the time series data to match zero mean function
+        self.timeseries.standardize()
+
+        # Convert time series data to PyTorch tensors
+        self.train_t = torch.tensor(self.timeseries.times)
+        self.train_y = torch.tensor(self.timeseries.rates)
+
+        # Set likelihood
+        self.likelihood = self.set_likelihood(white_noise)
+
+        # Inherit from ExactGP class
+        self.model = super(GaussianProcess, self).__init__(self.train_t, self.train_y, self.likelihood)
+
+        # Set mean and covariance
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = self.set_kernel(kernel_form)
+
+        # Training
+        if run_training:
+            self.train(train_iter, learn_rate, verbose)
+
+            # Calculate marginal log likelihood, BIC, and optimal hyperparameters
+            train_pred = self.model(self.train_t)
+            marg_log_like = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
+            self.opt_marg_log_like = marg_log_like(train_pred, self.train_y)
+            self.bic = self.bayesian_inf_crit(self.log_marg_like, self.model.num_params, len(self.train_t))
+            self.opt_hypers = self.model.covar_module.base_kernel.hypers
+    
+    def set_likelihood(self, white_noise):
+        
+        if self.timeseries.errors:
+            self.train_e = torch.tensor(self.timeseries.errors)
+            if white_noise:
+                likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(
+                    noise=self.train_e ** 2, 
+                    learn_additional_noise=white_noise
+                )
+
+        else:
+            if white_noise:
+                likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(
+                        noise=1e-8,
+                        learn_additional_noise=white_noise
+                )
+
+        return likelihood
+
+    def set_kernel(self, kernel_form: str):
+        """
+        Sets the covariance module (kernel) for the Gaussian Process model.
+
+        Parameters
+        ----------
+        kernel_form : str
+            Specifies the type of kernel to use. Options are:
+            - 'Matern12' for Matern kernel with nu=0.5
+            - 'Matern32' for Matern kernel with nu=1.5
+            - 'Matern52' for Matern kernel with nu=2.5
+            - 'RQ' for Rational Quadratic kernel
+            - 'RBF' for Radial Basis Function (RBF) kernel
+            - 'SpectralMixture, N' for Spectral Mixture kernel with N mixtures, where N is an integer
+
+        Raises
+        ------
+        ValueError
+            If an invalid kernel type is provided or if the mixture count is not valid.
+        """
+        # Parse kernel type and optional number of mixtures
+        if ',' in kernel_form:
+            kernel_type, num_mixtures_str = kernel_form.split(',')
+            kernel_type = kernel_type.strip()
+            try:
+                num_mixtures = int(num_mixtures_str.strip())
+            except ValueError:
+                raise ValueError(f"Invalid number of mixtures '{num_mixtures_str}' for Spectral Mixture kernel.")
+        else:
+            kernel_type = kernel_form.strip()
+        
+        # Kernel mapping with special handling for Spectral Mixture kernel
+        kernel_mapping = {
+            'Matern12': gpytorch.kernels.MaternKernel(nu=0.5),
+            'Matern32': gpytorch.kernels.MaternKernel(nu=1.5),
+            'Matern52': gpytorch.kernels.MaternKernel(nu=2.5),
+            'RQ': gpytorch.kernels.RQKernel(),
+            'RBF': gpytorch.kernels.RBFKernel(),
+            'SpectralMixture': gpytorch.kernels.SpectralMixtureKernel(num_mixtures=num_mixtures)
+        }
+        
+        # Assign kernel if type is valid
+        if kernel_type in kernel_mapping:
+            kernel = kernel_mapping[kernel_type]
+            if kernel_type == 'SpectralMixture':
+                kernel.initialize_from_data(self.train_t, self.train_y)
+
+            covar_module = gpytorch.kernels.ScaleKernel(kernel)
+            
+        else:
+            raise ValueError(f"Invalid kernel type '{kernel_type}'. Choose from {list(kernel_mapping.keys())}.")
+        
+        return covar_module
+    
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+    
+    def train(self, train_iter, learn_rate, verbose=False):
+        self.model.train()
+        self.likelihood.train()
+
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learn_rate)
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
+
+        for i in range(train_iter):
+            # Zero gradients from previous iteration
+            optimizer.zero_grad()
+
+            # Model output
+            output = self.model(self.train_t)
+
+            # Calc negative likelihood and backprop gradients
+            loss = -mll(output, self.train_y)
+            loss.backward()
+
+            if verbose:
+                if self.kernel_form == 'SpectralMixture':
+                    print('Iter %d/%d - Loss: %.3f   mixture_lengthscales: %.3f   mixture_weights: %s' % (
+                        i + 1, train_iter, loss.item(),
+                        self.model.covar_module.base_kernel.mixture_scales.item(),
+                        self.model.covar_module.base_kernel.mixture_weights.item()
+                    ))
+
+                else:
+                    if self.white_noise:
+                        print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+                            i + 1, train_iter, loss.item(),
+                            self.model.covar_module.base_kernel.lengthscale.item(),
+                            self.model.likelihood.noise.item()
+                        ))
+                    
+                    else:
+                        print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f' % (
+                            i + 1, train_iter, loss.item(),
+                            self.model.covar_module.base_kernel.lengthscale.item()
+                        ))
+
+            optimizer.step()
+
+    def bayesian_inf_crit(self, log_marg_like, num_params, num_data):
+        num_params = sum([p.numel() for p in self.model.parameters()])
+        bic = -2 * log_marg_like + num_params * np.log(num_data)
+        return bic
+    
+    def predict(self, pred_times, num_samples):
+        # Predictive posterior mode
+        self.model.eval()
+        self.likelihood.eval()
+
+        # Make predictions
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            pred_dist = self.likelihood(self.model(pred_times))
+            samples = pred_dist.rsample(sample_shape=torch.Size([num_samples]))
+
+        return samples
