0acce2e9b1158e3c50e3661280b419b7f6dbcd34
collinlewin <clewin@mit.edu>
2025-05-08 09:26:31 -0400

    Added SimulateLightCurve to __init__.

--- stela_toolkit/__init__.py
+++ stela_toolkit/__init__.py
@@ -1,6 +1,7 @@
 from .coherence import Coherence
 from .cross_spectrum import CrossSpectrum
 from .data_loader import LightCurve
+from .data_simulator import SimulateLightCurve
 from .frequency_binning import FrequencyBinning
 from .gaussian_process import GaussianProcess
 from .lag_energy_spectrum import LagEnergySpectrum
