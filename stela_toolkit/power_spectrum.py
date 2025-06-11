1532b00ac1e7b61aa26944360c99548f663b3d3a
collinlewin <clewin@mit.edu>
2025-06-11 10:16:12 -0400

    Modified fit docstring with model equations.

--- stela_toolkit/power_spectrum.py
+++ stela_toolkit/power_spectrum.py
@@ -208,7 +208,7 @@ class PowerSpectrum:
         return freqs, freq_widths, power_mean, power_std
     
     def fit(self, model_type='powerlaw', initial_params=None, lr=1e-3, max_iter=5000, tol=1e-6):
-        """
+        r"""
         Fit the binned power spectrum using a maximum likelihood approach based on the Gamma distribution.
 
         This method assumes that each binned PSD value represents the average of M independent
